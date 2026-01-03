dc77cb47a4917d4082d271cd307b139063e1c635
Behruz Jumanov <behruzjumanov55@gmail.com>
2026-01-03 23:20:10 +0500

    added new changes

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 //1-daraja:Prototip va Voris
 //1----
-const Animal = {
-    this:"Animal",
-    eat() {
-        console.log("Animal is eating");        
-    }
-};
-    const Dog = Object.create(Animal);
+// const Animal = {
+//     this:"Animal",
+//     eat() {
+//         console.log("Animal is eating");        
+//     }
+// };
+//     const Dog = Object.create(Animal);
   
-    Dog.type = "Kuchuk";
-    Dog.bark = function() {
-        console.log("Dog is kuchuk");
+//     Dog.type = "Kuchuk";
+//     Dog.bark = function() {
+//         console.log("Dog is kuchuk");
         
-    }
-    Dog.eat();
-    Dog.bark();
+//     }
+//     Dog.eat();
+//     Dog.bark();
 
     //2----
-    
+   class human  {
+  speak() {
+    console.log("bola o'rganyapti");
+  }
+};
+   
+     class Bola extends human {
+         u(){
+            console.log("bola vanihoyat gapiryapti");
+            
+        }
+     }
+const child = new Bola();
+child.speak();
+child.u();
+
+//3----
+let exampleObj = {};
+exampleObj.name = "Abror";
+exampleObj.age = 32;
+let result = exampleObj.hasOwnProperty("age");
+console.log(result);
+
+//4----
+function person() {
+    this.name = "Sardor",
+    this.age = 21,
+}
